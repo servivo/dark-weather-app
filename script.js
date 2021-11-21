cf785c1963283faae25beb10eea342fbca00e537
servivo <91890647+servivo@users.noreply.github.com>
2021-11-21 15:24:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function displayWeather(response) {
 let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 celsiusTemperature = response.data.main.temp;
